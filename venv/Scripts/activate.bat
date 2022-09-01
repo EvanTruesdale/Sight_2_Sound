4cd079e1971d5c78efcd481b19048545559db0da
Evan Truesdale <kayakcat@truecrew.net>
2022-08-31 19:54:09 -0700

    updated venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\kayak\OneDrive\Documents\Personal Files\Coding Projects\Sight_2_Sound\venv
+set VIRTUAL_ENV=C:\Users\Evan Truesdale\OneDrive\Documents\Personal Files\Coding Projects\Sight_2_Sound\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
